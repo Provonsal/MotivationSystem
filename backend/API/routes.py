c287543d36ebd975ecbe5ea37db428b5ced76d85
Provonsal <alecsw86@gmail.com>
2024-11-29 18:48:41 +0700

    Добавил рут сделок. Требуется еще одна функция от БД.

--- backend/API/routes.py
+++ backend/API/routes.py
@@ -14,14 +14,37 @@ async def get_login(user_id: int) -> str:
 async def get_id_by_login(login: str) -> str:
     ...
 
-async def get_salary_and_bonus(user_id: str, date: str) -> dict:
+async def get_salary_and_bonus(user_id: str, date: str) -> dict[
+        str: int, 
+        str: int
+    ]:
     ...
     return {"salary": ..., "bonus": ...}
 
 # тут должно вернуть лист с кучей словарей с данными людей, отсортированными по уменьшению.
-async def get_rating() -> list[dict]:
+async def get_rating() -> list[dict[
+        str: str, 
+        str: str, 
+        str: str, 
+        str: str, 
+        str: int, 
+        str: int
+    ]]:
     ...
     return [{"id": ..., "name": ..., "surname": ..., "lastname": ..., "salary": ..., "bonus": ...}, ...]
+
+# тут должно вернуть лист с кучей словарей с сделками, отсортированными от свежих к старым.
+async def get_deals(user_id: int) -> list[dict[
+        str: str, 
+        str: int, 
+        str: int, 
+        str: str, 
+        str: str, 
+        str: str
+    ]]:
+    ...
+    return [{"deal_id": ..., "sum": ..., "percent": ..., "deal_start_date": ..., "deal_end_date": ..., "selled": ...}, ...]
+
 
 # такой шаблон рутов
 @app.get("/password")
@@ -77,7 +100,7 @@ async def month_salary(body = Body()):
         return HTMLResponse(400)
 
 @app.get("/rating")
-async def month_salary(body = Body()):
+async def month_salary():
     
     """
     
@@ -97,3 +120,28 @@ async def month_salary(body = Body()):
         return {"result": "ok", "rating": rating}
     else:
         return HTMLResponse(400)
+    
+@app.get("/deals")
+async def employee_deals(body = Body()):
+    
+    """
+    
+    Функция выдает список сделок пользователя, упорядоченный по дате, от свежих, к старым. По умолчанию за текущий месяц.
+
+    :param body: json body
+    :type body: `fastapi.Body()`
+
+    :return: HTMLResponse 400 либо "result": "ok", [{"deal_id": ..., "sum": ..., "percent": ..., "deal_start_date": ..., "deal_end_date": ..., "selled": ...}, ...]}.
+    :rtype: `HTMLResponse 400` | `list[dict]`
+    
+    """
+
+    id: str = body["id"]
+
+    deals: list[dict] = get_deals(id)
+
+    if deals is not None:
+        return {"result": "ok", "deals": deals}
+    else:
+        return HTMLResponse(400)
+    
