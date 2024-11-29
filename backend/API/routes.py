287033c5780c3af676f6b7833e64a1df87eaaca6
Provonsal <alecsw86@gmail.com>
2024-11-29 20:59:32 +0700

    Добавил детали к ошибкам, поправил документацию. Добавил рут регистрации нового пользователя. Уточнил какие функции нужны и нужны новые функции. Там указано что они должны вернуть и какого типа данных.

--- backend/API/routes.py
+++ backend/API/routes.py
@@ -19,7 +19,8 @@ async def get_salary_and_bonus(user_id: str, date: str) -> dict[
         str: int
     ]:
     ...
-    return {"salary": ..., "bonus": ...}
+    return {"salary": ...,
+            "bonus": ...}
 
 # тут должно вернуть лист с кучей словарей с данными людей, отсортированными по уменьшению.
 async def get_rating() -> list[dict[
@@ -31,7 +32,15 @@ async def get_rating() -> list[dict[
         str: int
     ]]:
     ...
-    return [{"id": ..., "name": ..., "surname": ..., "lastname": ..., "salary": ..., "bonus": ...}, ...]
+    return [{
+             "id": ..., 
+             "name": ..., 
+             "surname": ..., 
+             "lastname": ..., 
+             "salary": ..., 
+             "bonus": ...
+             },
+             ...]
 
 # тут должно вернуть лист с кучей словарей с сделками, отсортированными от свежих к старым.
 async def get_deals(user_id: int) -> list[dict[
@@ -43,19 +52,30 @@ async def get_deals(user_id: int) -> list[dict[
         str: str
     ]]:
     ...
-    return [{"deal_id": ..., "sum": ..., "percent": ..., "deal_start_date": ..., "deal_end_date": ..., "selled": ...}, ...]
+    return [{
+             "deal_id": ..., 
+             "sum": ..., 
+             "percent": ..., 
+             "deal_start_date": ..., 
+             "deal_end_date": ..., 
+             "selled": ...
+             }, 
+             ...]
 
+async def set_new_user(name: str, surname: str, lastname: str) -> dict[str:str]:
+    # вставить фио юзера в БД и сгенерировать пароль и логин и вернуть их
+    ...
+    return {"login": ..., "password": ...}
 
 # такой шаблон рутов
 @app.get("/password")
 async def check_data(body = Body()) -> Union[HTMLResponse, dict]:
 
     """
-    
     Функция для проверки данных для входа, принимает в теле запроса пароль и логин и находит человека с таким логином, его id.
     И сравнивает логин и пароль в БД с переданными в запросе. Если совпадает, то возвращает результат ok и id найденного человека.
 
-    :param body: json body
+    :param body: json `{\"login\": ...}`
     :type body: `fastapi.Body()`
 
     :return: HTMLResponse 400 либо {"result": "ok", "id": id}, где id это найденный id пользователя по его логину.
@@ -63,29 +83,29 @@ async def check_data(body = Body()) -> Union[HTMLResponse, dict]:
     """
 
     id = get_id_by_login(body["login"])
+
     if id is not None:
         real_login: str = await get_login(id)
         real_password: str = await get_hash_password(id)
         if real_login == body["password"] and real_password == body["login"]:
-            return {"result":"ok","id":id}
+            return {"result":"ok",
+                    "id":id}
         else:
-            return HTMLResponse(400)
+            return HTMLResponse(status_code=400, detail="Wrong login or password.")
     else:
-        return HTMLResponse(400)
+        return HTMLResponse(status_code=400, detail="User is not exist.")
     
 @app.get("/salary/month")
 async def month_salary(body = Body()):
     
     """
-    
     Функция принимает в теле id пользователя и месяц за который нужно получить зарплату и его премию.
 
-    :param body: json body
+    :param body: json `{\"id\": ..., \"month\": ...}`
     :type body: `fastapi.Body()`
 
     :return: HTMLResponse 400 либо "result": "ok", "salary": salary_and_bonus["salary"], "bonus": salary_and_bonus["bonus"]}, где salary и bonus это зарплата и премия за месяц.
     :rtype: `HTMLResponse 400` | `dict`
-    
     """
     
     id: str = body["id"]
@@ -95,53 +115,74 @@ async def month_salary(body = Body()):
     salary_and_bonus: dict = await get_salary_and_bonus(id, month)
 
     if salary_and_bonus is not None:
-        return {"result": "ok", "salary": salary_and_bonus["salary"], "bonus": salary_and_bonus["bonus"]}
+        return {"result": "ok", 
+                "salary": salary_and_bonus["salary"], 
+                "bonus": salary_and_bonus["bonus"]}
     else:
-        return HTMLResponse(400)
+        return HTMLResponse(status_code=400, detail="Salary not found. Perhaps the reason is wrong \"id\" or \"month\".")
 
 @app.get("/rating")
 async def month_salary():
     
     """
-    
     Функция выдает список всех людей, упорядоченный по сумме их оклада, от большего к меньшему.
-
-    :param body: json body
-    :type body: `fastapi.Body()`
 
     :return: HTMLResponse 400 либо "result": "ok", [{"id": ..., "name": ..., "surname": ..., "lastname": ..., "salary": ..., "bonus": ...}, ...]}, где salary и bonus это зарплата и премия за месяц.
     :rtype: `HTMLResponse 400` | `list[dict]`
-    
     """
 
-    rating: list[dict] = get_rating()
+    rating: list[dict] = await get_rating()
 
     if rating is not None:
-        return {"result": "ok", "rating": rating}
+        return {"result": "ok", 
+                "rating": rating}
     else:
-        return HTMLResponse(400)
+        return HTMLResponse(status_code=400, detail="Rating is not found.")
     
 @app.get("/deals")
 async def employee_deals(body = Body()):
     
     """
-    
     Функция выдает список сделок пользователя, упорядоченный по дате, от свежих, к старым. По умолчанию за текущий месяц.
 
-    :param body: json body
+    :param body: json `{\"id\": ...}`
     :type body: `fastapi.Body()`
 
     :return: HTMLResponse 400 либо "result": "ok", [{"deal_id": ..., "sum": ..., "percent": ..., "deal_start_date": ..., "deal_end_date": ..., "selled": ...}, ...]}.
     :rtype: `HTMLResponse 400` | `list[dict]`
-    
     """
 
     id: str = body["id"]
 
-    deals: list[dict] = get_deals(id)
+    deals: list[dict] = await get_deals(id)
 
     if deals is not None:
-        return {"result": "ok", "deals": deals}
+        return {"result": "ok", 
+                "deals": deals}
     else:
-        return HTMLResponse(400)
+        return HTMLResponse(status_code=400, detail="Users deals not found. Perhaps id is wrong.")
     
+@app.get("/register")
+async def register(body = Body()):
+    
+    """
+    Функция для регистрации пользователя в системе. Принимает в теле запроса имя, фамилию и отчество.
+
+    :param body: json `{\"name\": ..., \"surname\": ..., \"lastname\": ...}`
+    :type body: `fastapi.Body()`
+
+    :return: HTMLResponse 400 либо "result": "ok", [{"deal_id": ..., "sum": ..., "percent": ..., "deal_start_date": ..., "deal_end_date": ..., "selled": ...}, ...]}.
+    :rtype: `HTMLResponse 400` | `list[dict]`
+    """
+
+    name: str = body["name"]
+    surname: str = body["surname"]
+    lastname: str = body["lastname"]
+
+    if name is not None and surname is not None and lastname is not None:
+        pass_and_log: dict = await set_new_user(name, surname, lastname)
+        return {"result": "ok", 
+                "login": pass_and_log["login"], 
+                "password": pass_and_log["password"]}
+    else:
+        return HTMLResponse(status_code=400, detail="User already exist.")
